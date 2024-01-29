86b7d0e853ce2d4170911fa6429d3cb2e0f7dd8a
Rushil Velupula <98041200+rvelupul@users.noreply.github.com>
2024-01-29 18:59:32 -0500

    Create prefix_sum.h

---
+++ prefix_sum.h
@@ -0,0 +1,11 @@
+#ifndef __PREFIX_SUM_H
+#define __PREFIX_SUM_H
+
+
+int prefix_sum(int ary[], int length);
+
+bool non_neg_prefix_sum(int ary[], int length);
+bool non_pos_prefix_sum(int ary[], int length);
+
+
+#endif
